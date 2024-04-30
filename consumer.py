014f0bc516cec55a65463edc6c597bc17be8994f
Guilherme Diel <guiddiel@gmail.com>
2024-04-30 10:57:59 -0300

    first version working

--- consumer.py
+++ consumer.py
@@ -1,27 +1,77 @@
-#Server
 import pika
-
-QUEUE_NAME_LOW = 'fila_sdi_baixa'
-QUEUE_NAME_HIGH = 'fila_sdi_alta'
-
-# Conecta-se ao servidor RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-
-# Cria uma fila chamada 'fila_teste'
-channel.queue_declare(queue=QUEUE_NAME_LOW)
-channel.queue_declare(queue=QUEUE_NAME_HIGH)
-
-# Função de callback para processar mensagens recebidas
-def callback(ch, method, properties, body):
-    print("Mensagem recebida:", body.decode())
-
-# Registra a função de callback para a fila
-channel.basic_consume(queue=QUEUE_NAME_LOW, on_message_callback=callback, auto_ack=True)
-channel.basic_consume(queue=QUEUE_NAME_HIGH, on_message_callback=callback, auto_ack=True)
+import time
+import threading
 
 
-print('Aguardando mensagens. Pressione CTRL+C para sair.')
+class Job:
+    def __init__(self, exec_time, resources, priority, id, start_at):
+        self.exec_time = int(exec_time)
+        self.resources = int(resources)
+        self.priority = int(priority)
+        self.id = id
+        self.start_at = start_at
+        self.end_at = start_at + self.exec_time
 
-# Começa a consumir mensagens
-channel.start_consuming()
+    def __str__(self):
+        return f"ID:{self.id};Exec time:{self.exec_time};Resources:{self.resources};Priority{self.priority};Start at{self.start_at};End at:{self.end_at}"
+
+
+class Server:
+    def __init__(self):
+        self.MAX_RESOURCES = 200
+        self.cur_used_res = 0
+        self.id_job = 0
+        self.scheduled = []
+
+    def callback(self, ch, method, properties, body):
+        exec_time, resources, priority = body.decode().split(';')
+        resources = int(resources)
+
+        # Can be scheduled
+        if self.cur_used_res + resources > self.MAX_RESOURCES:
+            print(f"There is not space to schedule the job with {resources} resources.")
+            return
+
+        job = Job(exec_time, resources, priority, self.id_job, time.time())
+        self.scheduled.append(job)
+
+        self.id_job += 1
+        self.cur_used_res += resources
+
+        thread = threading.Thread(target=self.handle_job, args=(job,))
+        thread.start()
+
+        print(f"Resources used: {self.cur_used_res} | Resources available: {self.MAX_RESOURCES - self.cur_used_res}")
+
+    def handle_job(self, job):
+        time.sleep(job.exec_time)
+        self.cur_used_res -= job.resources
+
+
+    def start(self):
+        QUEUE_LOW = 'fila_sdi_baixa'  # 0
+        QUEUE_HIGH = 'fila_sdi_alta'  # 1
+
+        # Conecta-se ao servidor RabbitMQ
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+
+        # low_info = channel.queue_declare(queue=QUEUE_LOW)
+
+        while True:
+            # Priorizar Q_HIGH
+            channel.basic_consume(queue=QUEUE_HIGH, on_message_callback=self.callback, auto_ack=True)
+            high_info = channel.queue_declare(queue=QUEUE_HIGH, passive=True)
+
+            if high_info.method.message_count == 0:
+                channel.basic_consume(queue=QUEUE_LOW, on_message_callback=self.callback, auto_ack=True)
+
+            print('Aguardando mensagens. Pressione CTRL+C para sair.')
+
+            # Começa a consumir mensagens
+            channel.start_consuming()
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
--- producer.py
+++ producer.py
@@ -1,23 +1,42 @@
 # Cliente
 import pika
+import time
+import random
 
-QUEUE_NAME_LOW = 'fila_sdi_baixa'
-QUEUE_NAME_HIGH = 'fila_sdi_alta'
+QUEUE_LOW = 'fila_sdi_baixa'  # 0
+QUEUE_HIGH = 'fila_sdi_alta'  # 1
+
+MAX_TIME = 20
+MAX_RESOURCES = 10
+
+# msg = time, resources, priority
 
 # Conecta-se ao servidor RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+channel.queue_declare(queue=QUEUE_LOW)
+channel.queue_declare(queue=QUEUE_HIGH)
 
-# Declara a fila 'fila_teste'
-channel.queue_declare(queue=QUEUE_NAME_LOW)
-channel.queue_declare(queue=QUEUE_NAME_HIGH)
+oi = 1 if 1 == 1 else 2
 
-# Envia uma mensagem para a fila
-channel.basic_publish(exchange='', routing_key=QUEUE_NAME_LOW, body='Olá, mundo_1!')
-channel.basic_publish(exchange='', routing_key=QUEUE_NAME_HIGH, body='Olá, mundo_2!')
+while True:
+    try:
+        execution_time = random.randint(1, MAX_TIME)
+        resources = random.randint(1, MAX_RESOURCES)
+        priority = random.randint(0, 1)
 
+        msg = f"{execution_time};{resources};{priority}"
 
-print("Mensagem enviada.")
+        # Envia uma mensagem para a fila
+        channel.basic_publish(exchange='', routing_key=QUEUE_LOW if priority == 0 else QUEUE_HIGH, body=msg)
+        print(f"Mensagem enviada: {msg}")
 
-# Fecha a conexão
-connection.close()
+        time.sleep(0.1)
+        
+    except Exception:
+        print("Falha ao enviar mensagem.")
+        try:
+            connection.close()
+        except  Exception as e:
+            print("Falha ao fechar a conexão.")
+        break
